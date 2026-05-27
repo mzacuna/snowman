2d10295daf074aba245f5ed35e4027e1523b3952
Martín Zamorano <martin@mzamorano.com>
2026-05-26 18:12:47 -0700

    home-manager: turn on backupFileExtension

--- modules/common/home-manager.nix
+++ modules/common/home-manager.nix
@@ -4,5 +4,9 @@
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
+
+    # Rename pre-existing files instead of failing activation when home-manager
+    # wants to manage a path that already exists.
+    backupFileExtension = "backup";
   };
 }
