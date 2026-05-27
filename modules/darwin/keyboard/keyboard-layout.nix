1c3075bdc5af56da58c0ce2577ec9cfa769237e2
Martín Zamorano <martin@mzamorano.com>
2026-05-26 18:02:51 -0700

    darwin(keyboard-layout.nix): minor refactor

--- modules/darwin/keyboard/keyboard-layout.nix
+++ modules/darwin/keyboard/keyboard-layout.nix
@@ -1,15 +1,20 @@
-{ lib, inputs, ... }:
+{ inputs, ... }:
 
 {
   homebrew.casks = [ "ukelele" ];
 
+  # Function form so the inner `lib` is home-manager's module lib, which carries
+  # `lib.hm`.
   home-manager.sharedModules = [
-    {
-      home.activation.installKeylayout = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-        mkdir -p "$HOME/Library/Keyboard Layouts"
-        cp -f ${inputs.tangent}/mac/Tangent-Gallium.keylayout "$HOME/Library/Keyboard Layouts/Tangent Gallium.keylayout"
-        cp -f ${inputs.tangent}/mac/Kuntem-JQ.keylayout "$HOME/Library/Keyboard Layouts/Kuntem-JQ.keylayout"
-      '';
-    }
+    (
+      { lib, ... }:
+      {
+        home.activation.installKeylayout = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+          mkdir -p "$HOME/Library/Keyboard Layouts"
+          cp -f ${inputs.tangent}/mac/Tangent-Gallium.keylayout "$HOME/Library/Keyboard Layouts/Tangent Gallium.keylayout"
+          cp -f ${inputs.tangent}/mac/Kuntem-JQ.keylayout "$HOME/Library/Keyboard Layouts/Kuntem-JQ.keylayout"
+        '';
+      }
+    )
   ];
 }
