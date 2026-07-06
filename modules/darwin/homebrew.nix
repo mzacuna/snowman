45ef399f534390e91314a417a39c25490cb13c34
Martín Zamorano <martin@mzamorano.com>
2026-07-05 17:38:50 -0700

    fix(darwin): trust Emacs Homebrew tap

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,22 +1,40 @@
 {
   config,
   inputs,
+  lib,
   username,
   ...
 }:
 
 let
+  inherit (lib.attrsets) attrNames;
+  inherit (lib.lists) elem;
+
   homebrewEnv = {
     HOMEBREW_NO_ANALYTICS = "1";
     HOMEBREW_NO_ENV_HINTS = "1";
     HOMEBREW_NO_UPDATE_REPORT_NEW = "1";
   };
+
+  trustedTaps = [
+    "jimeh/homebrew-emacs-builds"
+  ];
+
+  mkTap =
+    name:
+    if elem name trustedTaps then
+      {
+        inherit name;
+        trusted = true;
+      }
+    else
+      name;
 in
 {
   homebrew = {
     enable = true;
 
-    taps = builtins.attrNames config.nix-homebrew.taps;
+    taps = config.nix-homebrew.taps |> attrNames |> map mkTap;
 
     onActivation = {
       cleanup = "uninstall";
