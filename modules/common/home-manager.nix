cc3ec9f865c2542ad8345d51e411e627fe6d7173
Martín Zamorano <martin@mzamorano.com>
2026-07-09 08:59:31 -0700

    feat: set XDG session variables

--- modules/common/home-manager.nix
+++ modules/common/home-manager.nix
@@ -1,4 +1,21 @@
-{ config, username, ... }:
+{
+  config,
+  lib,
+  system,
+  username,
+  ...
+}:
+
+let
+  homeDirectory = config.users.users.${username}.home;
+  isDarwin = lib.hasSuffix "-darwin" system;
+  xdgSessionVariables = {
+    XDG_CACHE_HOME = "${homeDirectory}/.cache";
+    XDG_CONFIG_HOME = "${homeDirectory}/.config";
+    XDG_DATA_HOME = "${homeDirectory}/.local/share";
+    XDG_STATE_HOME = "${homeDirectory}/.local/state";
+  };
+in
 
 {
   home-manager = {
@@ -10,6 +27,13 @@
     # wants to manage a path that already exists.
     backupFileExtension = "backup";
 
-    users.${username}.home.homeDirectory = config.users.users.${username}.home;
+    users.${username}.home = {
+      homeDirectory = homeDirectory;
+
+      sessionVariables = xdgSessionVariables;
+    };
   };
+}
+// lib.optionalAttrs isDarwin {
+  launchd.user.envVariables = xdgSessionVariables;
 }
